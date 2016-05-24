cbd75358f3aa86ee230c5caa685191d31ef7060b
Christian Palmhøj Nielsen <cpalmhoj@gmail.com>
2016-05-25 00:03:19 +0200

    Serve static files via nginx (shared from web)

--- web/adventurelookup/urls.py
+++ web/adventurelookup/urls.py
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
-
-# Serve static files during development
-urlpatterns += staticfiles_urlpatterns()
