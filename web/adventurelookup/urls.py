1166a7e545d74ae8ccd72099dc8f94e7a1088c3b
Pejter <piotrluczak1995@gmail.com>
2016-05-24 16:15:53 +0200

    Serve static fles during development

--- web/adventurelookup/urls.py
+++ web/adventurelookup/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+# Serve static files during development
+urlpatterns += staticfiles_urlpatterns()
