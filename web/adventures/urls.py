4a002414684979de0131093d4bc04247f9c2d504
whonut <dylanlee.evans95@gmail.com>
2016-06-01 18:29:45 +0100

    Fix URL routing in adventures

--- web/adventures/urls.py
+++ web/adventures/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^adventure/(.+)$', views.get_adventure_by_id),
+    url(r'^adventure/(.+)$', views.:adventure_by_id),
 ]
