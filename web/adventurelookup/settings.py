fd9c832ccd8fb999e23bce400d92855f795fbcca
Pejter <piotrluczak1995@gmail.com>
2016-06-01 15:17:53 +0200

    Cache support
    Added pylibmc to requirements
    Added Caching section in settings
    Added memcached container

--- web/adventurelookup/settings.py
+++ web/adventurelookup/settings.py
@@ -89,6 +89,16 @@ DATABASES = {
     }
 }
 
+# Cache
+# https://docs.djangoproject.com/en/1.9/topics/cache/
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'LOCATION': 'memcached:11211',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
