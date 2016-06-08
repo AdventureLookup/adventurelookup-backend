d2c245302f63561e4f6c6cc1e603e60d289015cf
Pejter <piotrluczak1995@gmail.com>
2016-06-08 18:51:27 +0200

    Fix adventure urls

--- web/adventures/urls.py
+++ web/adventures/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [url(r'^adventure/(?P<adventure_id>\d+)$', views.AdventureById.as_view(), name="adventure-by-id"), ]
+urlpatterns = [url(r'adventure/(?P<adventure_id>\d+)$', views.AdventureById.as_view(), name="adventure-by-id"), ]
