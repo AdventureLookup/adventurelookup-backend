99272cfae1e9477077a38475189184005e0b2bf9
whonut <dylanlee.evans95@gmail.com>
2016-06-13 19:36:42 +0100

    Add adventure editing via PUT requests
    No attempt is made at authentication, nor at proper CSRF-handling.
    The assumption is made that authentication will be handled by a
    parent view.

--- web/adventures/views.py
+++ web/adventures/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
@@ -15,3 +17,14 @@ class AdventureById(View):
         adventure = get_object_or_404(models.Adventure, pk=self.adventure_id)
         data = model_to_dict(adventure)
         return JsonResponse(data)
+
+    def put(self, request):
+        new_data = json.loads(request.body.decode('utf-8'))
+
+        adventure_qset = models.Adventure.objects.filter(pk=self.adventure_id)
+        if not adventure_qset:
+            return JsonResponse({}, status=404,
+                                reason='No Adventure matches the given query.')
+        adventure_qset.update(**new_data)
+        adventure_data = model_to_dict(adventure_qset[0])
+        return JsonResponse(adventure_data)
